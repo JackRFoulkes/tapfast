51f0bd4f00bfdcc8ccb8ce64d66eeadb5b777302
Jack Foulkes <jackrfoulkesapps@gmail.com>
2021-02-27 21:35:04 +0000

    main menu base added

---
+++ tapfast/Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    // Awake
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Public methods exposed to the editor
+    public void PlayGame(int mode)
+    {
+        SceneManager.LoadScene("Play");
+    }
+}
